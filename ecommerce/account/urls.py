081bf75bc3666a486694e85d2c22ff3fefcdd7a8
kedrick1 <kedricklavigne@gmail.com>
2024-10-13 23:23:11 -0400

    added password reset functionality

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+from django.contrib.auth import views as auth_views #be url in a view, unique django built in django view reset
+
 urlpatterns = [
     path('register', views.register, name='register'),
 
@@ -26,6 +29,23 @@ urlpatterns = [
     path('profile-management', views.profile_management, name='profile-management'),
     
     path('delete-account', views.delete_account, name='delete-account'),
+
+
+
+
+    #password management urls/views
+    
+    #submit our email form where we say what our email is to receive the new link
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name="account/password/password-reset.html"), name='reset_password'), #user submit our email form
+
+    #success message , email will be sent to reset password
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name="account/password/password-reset-sent.html"), name='password_reset_done'), #templates will be in parentisis
+
+    #password reset link email
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password/password-reset-form.html"), name='password_reset_confirm'), 
+
+    #success message password was change
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name="account/password/password-reset-complete.html"), name='password_reset_complete'),
 
 
 
